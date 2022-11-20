fe49e2c13b1c23a548555fe3a685d79a5bab0c37
Grace Ocampina <grace.ocampina@ucsf.edu>
2022-11-20 09:51:41 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
+
+var i=0
+
+document.getElementById('start').onclick= showQuestions
+
+
+function showQuestions(){
+    if (i< questions.length){
+        document.getElementById("questions").innerHTML=
+        `
+        <h3>${questions[i].title}</h3>
+        <button>${questions[i].choices[0]}</button>
+        <button>${questions[i].choices[1]}</button>
+        <button>${questions[i].choices[2]}</button>
+        <button>${questions[i].choices[3]}</button>
+        
+        `
+    }
+}
+
+document.getElementById('questions').onclick= function(e){
+    console.log(e.target.innerText)
+    if(questions[i].answer === e.target.innerText ){
+        document.getElementById("check") = "Correct"
+    }else { document.getElementById("check") = "Wrong"}
+
+    i++;
+    showQuestions()
+}
