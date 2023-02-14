5b0144f227ab39a46fe6bdfb7b0106346128aa47
Grace Ocampina <grace.ocampina@ucsf.edu>
2023-02-13 16:18:00 -0800

    updated questions, added comments

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var questions = [
+function startGame() {
+
+}
+
+
+function 
+
+
+
+
+
+
+
+
+
+// List of questions variables
+
+let questions = [
   {
-    title: 'Commonly used data types DO NOT include:',
+    Question 1: 'Commonly used data types DO NOT include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
     answer: 'alerts',
   },
   {
-    title: 'The condition in an if / else statement is enclosed within ____.',
+    Question 2: 'The condition in an if / else statement is enclosed within ____.',
     choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
     answer: 'parentheses',
   },
   {
-    title: 'Arrays in JavaScript can be used to store ____.',
+    Question 3: 'Arrays in JavaScript can be used to store ____.',
     choices: [
       'numbers and strings',
       'other arrays',
@@ -20,13 +37,13 @@ var questions = [
     answer: 'all of the above',
   },
   {
-    title:
+    Question 4:
       'String values must be enclosed within ____ when being assigned to variables.',
     choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
     answer: 'quotes',
   },
   {
-    title:
+    Question 5:
       'A very useful tool used during development and debugging for printing content to the debugger is:',
     choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
     answer: 'console.log',
@@ -60,4 +77,4 @@ document.getElementById('questions').onclick= function(e){
 
     i++;
     showQuestions()
-}
+}*/
