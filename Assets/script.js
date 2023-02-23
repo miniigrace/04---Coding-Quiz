e70d3bbd17a9de9f1645add4908c2f19286668ec
Grace Ocampina <grace.ocampina@ucsf.edu>
2023-02-22 23:30:45 -0800

    added elements on script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,67 +1,35 @@
+// lists of required elements
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+var questionDiv = document.getElementById("questionDiv");
+var questionTitle = document.getElementById("questionTitle");
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+var answerCheck = document.getElementById("answerCheck");
+
+var summary = document.getElementById("summary");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var initialInput = document.getElementById("initialInput");
+var everything = document.getElementById("everything");
+
+var highScoreSection = document.getElementById("highScoreSection");
+var finalScore = document.getElementById("finalScore");
+
+var goBackBtn = document.getElementById("goBackBtn");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
+var viewHighScore = document.getElementById("viewHighScore");
+var listOfHighScores = document.getElementById("listOfHighScores");
 
 
-// List of questions variables
+var correctAns = 0;
+var questionNum = 0;
+var scoreResult;
+var questionIndex = 0;
 
-var questions = [
-  {
-    Question 1: 'Commonly used data types DO NOT include:',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    answer: 'alerts',
-  },
-  {
-    Question 2: 'The condition in an if / else statement is enclosed within ____.',
-    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-    answer: 'parentheses',
-  },
-  {
-    Question 3: 'Arrays in JavaScript can be used to store ____.',
-    choices: [
-      'numbers and strings',
-      'other arrays',
-      'booleans',
-      'all of the above',
-    ],
-    answer: 'all of the above',
-  },
-  {
-    Question 4:
-      'String values must be enclosed within ____ when being assigned to variables.',
-    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-    answer: 'quotes',
-  },
-  {
-    Question 5:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
-    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-    answer: 'console.log',
-  },
-];
-
-var i=0
-
-document.getElementById('start').onclick= showQuestions
-
-
-function showQuestions(){
-    if (i< questions.length){
-        document.getElementById("questions").innerHTML=
-        `
-        <h3>${questions[i].title}</h3>
-        <button>${questions[i].choices[0]}</button>
-        <button>${questions[i].choices[1]}</button>
-        <button>${questions[i].choices[2]}</button>
-        <button>${questions[i].choices[3]}</button>
-        
-        `
-    }
-}
-
-document.getElementById('questions').onclick= function(e){
-    console.log(e.target.innerText)
-    if(questions[i].answer === e.target.innerText ){
-        document.getElementById("check") = "Correct"
-    }else { document.getElementById("check") = "Wrong"}
-
-    i++;
-    showQuestions()
-}*/
