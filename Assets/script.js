b31224b18ebdec108b62294b35d6c8f5002b0077
Grace Ocampina <grace.ocampina@ucsf.edu>
2023-02-22 23:22:50 -0800

    fixed invalid codes on index.html, added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,8 @@
-function startGame() {
-
-}
-
-
-function 
-
-
-
-
-
-
-
 
 
 // List of questions variables
 
-let questions = [
+var questions = [
   {
     Question 1: 'Commonly used data types DO NOT include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
